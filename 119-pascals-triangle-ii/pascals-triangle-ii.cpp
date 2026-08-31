d766336178eaca4c55ae3c3d4d523ba3d87cbc3f
Aniket Kumar <aniketgupta2801@gmail.com>
2026-09-01 00:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (74.75%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans;
+        ans.push_back(1);
+        long long res = 1;
+        for(int i=0; i<rowIndex; i++){
+            res = res * (rowIndex - i );
+            res = res/(i+1);
+            ans.push_back(res);
+        }
+        return ans;
+        
+    }
+};
