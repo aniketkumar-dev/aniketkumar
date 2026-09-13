8af50032866b541bc7907aa862274d1b69e81437
Aniket Kumar <aniketgupta2801@gmail.com>
2026-09-13 18:55:47 +0530

    Time: 4 ms (10.61%) | Memory: 71.8 MB (52.34%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,11 +5,12 @@ public:
         int ans = INT_MIN;
         int sum = 0;
         for(int i=0; i<n; i++){
-            sum = sum + nums[i];
-            ans = max(ans, sum);
+            sum += nums[i];
+            ans =  max (ans, sum);
             if(sum < 0){
                 sum = 0;
             }
+           
         }
         return ans;
         
