91b85e99f12b783de39523c8d65823bc2b8c98e0
Aniket Kumar <aniketgupta2801@gmail.com>
2026-09-19 22:12:36 +0530

    Time: 35 ms (35.83%) | Memory: 16.8 MB (9.88%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        sort(nums.begin(), nums.end());
+
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    // i se j-1 tak sabke saath triangle banega
+                    ans += j - i;
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
